50388fcfbdf65bd2db74d7078364d621f7ce5b37
lxj <lxj>
2022-03-21 10:12:54 +0800

    修正打包问题

--- build.bat
+++ build.bat
@@ -4,18 +4,18 @@ SET CGO_ENABLED=0
 SET GOARCH=amd64
 
 SET GOOS=darwin
-go build -o FastFileSafe-macos
+go build -o build-exe\FastFileSafe-macos
 
 SET GOOS=linux
-go build -o FastFileSafe-linux
+go build -o build-exe\FastFileSafe-linux
 
 
 SET GOOS=windows
-go build -ldflags "-H windows -w"
+go build -ldflags "-w" -o build-exe\FastFileSafe.exe
 
 SET GOOS=linux
 SET GOARCH=arm64
-go build -o FastFileSafe-linux-arm64
+go build -o -o build-exe\FastFileSafe-linux-arm64
 
 
 
